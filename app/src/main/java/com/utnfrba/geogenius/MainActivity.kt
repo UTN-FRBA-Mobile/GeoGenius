cabfb2603126ccd660888f13fe62828de6f07b52
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-09-28 15:19:11 -0300

    update names

--- app/src/main/java/com/utnfrba/geogenius/MainActivity.kt
+++ app/src/main/java/com/utnfrba/geogenius/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GeoGeniusTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "UTN",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hola $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GeoGeniusTheme {
-        Greeting("Android")
+        Greeting("UTN")
     }
 }
