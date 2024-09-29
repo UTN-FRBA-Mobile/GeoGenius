57006cef861cdd59f394193c78a22129f219a2ee
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-09-29 11:39:33 -0300

    change on click

--- app/src/main/java/com/utnfrba/geogenius/BottomNavigationBar.kt
+++ app/src/main/java/com/utnfrba/geogenius/BottomNavigationBar.kt
@@ -1,37 +1,49 @@
 package com.utnfrba.geogenius
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.utnfrba.geogenius.ui.theme.GeoGeniusTheme
 
+
+
+
 @Composable
 fun BottomNavigationBar() {
-    NavigationBar() {
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.LocationOn, "Filters") },
-            selected = true,
-            onClick = {},
-            label = { Text("Filters") }
-        )
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Filters", "Map", "Bookmarks")
+    val selectedIcons = listOf(Icons.Filled.Home, Icons.Filled.LocationOn, Icons.Filled.Star)
+    val unselectedIcons =
+        listOf(Icons.Outlined.Home, Icons.Outlined.LocationOn, Icons.Outlined.Star)
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
+                    contentDescription = item)
+                   },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+                label = { Text(item) }
+            )
+        }
         // Ask why not all icons are here
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.LocationOn, "Location") },
-            selected = false,
-            onClick = {},
-            label = { Text("Location") }
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Default.LocationOn, "Bookmarks") },
-            selected = false,
-            onClick = {},
-            label = { Text("Bookmarks") }
-        )
     }
 }
 
