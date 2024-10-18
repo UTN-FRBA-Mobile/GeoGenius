26e01115a0281c4ca0c1beda9aab9e5427b3ba14
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-10-18 09:47:16 -0300

    add background

--- app/src/main/java/com/utnfrba/geogenius/widget/GlanceAppWidget.kt
+++ app/src/main/java/com/utnfrba/geogenius/widget/GlanceAppWidget.kt
@@ -2,11 +2,17 @@ package com.utnfrba.geogenius.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.glance.Button
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
 
 class GlanceAppWidget: GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = GeoGeniusWidget()
@@ -26,10 +32,15 @@ class GeoGeniusWidget : GlanceAppWidget() {
 
     @Composable
     private fun Content() {
-        Button(
-            text = "Home",
-            onClick = {}
-        )
+        Column(
+            modifier = GlanceModifier.fillMaxSize().background(Color.White),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                text = "Hello",
+                onClick = {}
+            )
+        }
     }
-
 }
