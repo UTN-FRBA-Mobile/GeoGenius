e57d34bc0351c379a81d29576db1b8679aeb9c03
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-11-10 14:18:58 -0300

    use textbutton for results

--- app/src/main/java/com/utnfrba/geogenius/screens/maps/SearchBar.kt
+++ app/src/main/java/com/utnfrba/geogenius/screens/maps/SearchBar.kt
@@ -1,17 +1,20 @@
 package com.utnfrba.geogenius.screens.maps
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -40,18 +43,25 @@ fun SearchBarComponent(modifier: Modifier = Modifier) {
     ) {
         LazyColumn {
             val filtered = countriesList.filter {  country ->
-                country.uppercase().contains(searchQuery.trim().uppercase())
+                if (searchQuery != "") {
+                    country.uppercase().contains(searchQuery.trim().uppercase())
+                } else false
             }
             items(filtered.size) { country ->
-                Text(
-                    text = filtered[country],
-                    modifier = modifier.padding(
-                        start = 8.dp,
-                        top = 4.dp,
-                        end = 8.dp,
-                        bottom = 4.dp
+                TextButton(
+                    onClick = {},
+                    Modifier.background(Color.Transparent)
+                ) {
+                    Text(
+                        text = filtered[country],
+                        modifier = modifier.padding(
+                            start = 8.dp,
+                            top = 4.dp,
+                            end = 8.dp,
+                            bottom = 4.dp
+                        )
                     )
-                )
+                }
             }
         }
     }
